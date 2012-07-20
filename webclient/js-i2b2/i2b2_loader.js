289a3037f5d8c16a2710118e6d0d9fb648c82bbb
badagarla <none@none>
2012-07-20 11:14:54 -0500

    Deleting the lines related to proj req plugin (#1189)

--- webclient/js-i2b2/i2b2_loader.js
+++ webclient/js-i2b2/i2b2_loader.js
@@ -79,12 +79,6 @@ i2b2.hive.tempCellsList = [
 		   roles: [ "DATA_LDS", "DATA_DEID", "DATA_PROT" ],
 		   forceDir: "cells/plugins/standard"
 		},
-//		{ code: "ProjectRequest",
-//		  forceLoading: true,
-//		  forceConfigMsg: { params: [] },
-//		   roles: [ "DATA_LDS", "DATA_DEID", "DATA_PROT" ],
-//		  forceDir: "cells/plugins/standard"
-//		},
                 { code: "KaplanMeierStat",
 		  forceLoading: true,
 		  forceConfigMsg: { params: [] },
