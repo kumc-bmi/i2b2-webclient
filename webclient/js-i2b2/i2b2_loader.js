5578d5f6e49a1d91785db7700aa62a670e607224
Nathan Graham <ngraham@kumc.edu>
2016-08-25 13:33:26 -0500

    Comment out the survival analysis plugins (#4230)
    --HG--
    branch : remove_survival_plugins_4230

--- webclient/js-i2b2/i2b2_loader.js
+++ webclient/js-i2b2/i2b2_loader.js
@@ -84,25 +84,27 @@ i2b2.hive.tempCellsList = [
 		   roles: [ "DATA_LDS", "DATA_DEID", "DATA_PROT" ],
 		   forceDir: "cells/plugins/community"
 		},
-*/
+
 		{ code: "KaplanMeierStat",
 		  forceLoading: true,
 		  forceConfigMsg: { params: [] },
 		  roles: [ "DATA_LDS", "DATA_DEID", "DATA_PROT" ],
 		  forceDir: "cells/plugins/fsm"
-                },		
+                },
+*/
                 { code: "DFBuilder",
 		  forceLoading: true,
 		  forceConfigMsg: { params: [] },
 		  roles: [ "HERON_ANALYSIS" ],
 		  forceDir: "cells/plugins/fsm"
-                },		
+                }/*,		
                 { code: "CopperJohnStat",
 		  forceLoading: true,
 		  forceConfigMsg: { params: [] },
 		  roles: [ "DATA_LDS", "DATA_DEID", "DATA_PROT" ],
 		  forceDir: "cells/plugins/fsm"
-                }	
+                }
+*/	
 	];
 // ================================================================================================== //
 //     ^^
